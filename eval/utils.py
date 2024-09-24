8123675bdaed8bce77d3975c573644254b3db461
root <b22ai063@iitj.ac.in>
2024-09-24 14:34:08 +0530

    Word Cloud Generation
    Signed-off-by: root <b22ai063@iitj.ac.in>

---
+++ eval/utils.py
@@ -0,0 +1,7 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt 
+
+def generate_wordcloud(text: str, file_name: str):
+    wc = WordCloud(background_color="white", colormap='Oranges', width=300, height=300).generate(text)
+    plt.axis("off")
+    plt.savefig(f"word_cloud_{file_name}.png")
